8b6a0c0eb65fe3b111f4577363dedaf1a0fd3a09
Sarvagya Sagar <iamsarvagyaa@gmail.com>
2023-01-04 12:38:54 +0000

    tests: add params & queries for grpcurl

--- .script/grpcTests/test.sh
+++ .script/grpcTests/test.sh
@@ -3,4 +3,20 @@
 NODE=""
 GRPCURL=""
 
+# params
+grpcurl -plaintext localhost:9090 list
+grpcurl -plaintext localhost:9090 describe identities.queries.identity.Query
+grpcurl -plaintext localhost:9090 describe classifications.queries.classification.Query
+grpcurl -plaintext localhost:9090 describe assets.queries.asset.Query
+grpcurl -plaintext localhost:9090 describe maintainers.queries.maintainer.Query
+grpcurl -plaintext localhost:9090 describe metas.queries.meta.Query
+grpcurl -plaintext localhost:9090 describe splits.queries.split.Query
+grpcurl -plaintext localhost:9090 describe splits.queries.ownable.Query
+grpcurl -plaintext localhost:9090 describe orders.queries.order.Query
+
+# tests
+echo -e "\nendpoint: cosmos.bank.v1beta1.Query/AllBalances\n"
 grpcurl -plaintext -d '{"address":"mantle1pkkayn066msg6kn33wnl5srhdt3tnu2vuet86j"}' localhost:9090 cosmos.bank.v1beta1.Query/AllBalances
+
+echo -e "\nendpoint: cosmos.bank.v1beta1.Query/Balance\n"
+grpcurl -plaintext -d '{"address":"mantle1pkkayn066msg6kn33wnl5srhdt3tnu2vuet86j","denom":"stake"}' localhost:9090 cosmos.bank.v1beta1.Query/Balance
